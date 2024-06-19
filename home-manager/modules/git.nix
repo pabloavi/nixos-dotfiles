38ab9d4ea9ef649675320eb928caba81145fbe7b
Pablo <pavilesm1@gmail.com>
2024-06-19 18:28:24 +0200

    idk

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    git-credential-oauth
-  ];
+  home.packages = with pkgs; [ ];
 
   programs.git = {
     enable = true;
     userName = "Pablo";
     userEmail = "pavilesm1@gmail.com";
   };
+
+  programs.git-credential-oauth.enable = true;
 }
